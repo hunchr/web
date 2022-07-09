965423e6e519c4bd62d59ae700fa187a971c4555
hunchr <76159444+hunchr@users.noreply.github.com>
2022-07-09 15:04:00 +0200

    nav/form

--- _/index.php
+++ _/index.php
@@ -1,3 +1,11 @@
 <?php
+//  $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+//  $acceptLang = ['fr', 'it', 'en']; 
+//  $lang = in_array($lang, $acceptLang) ? $lang : 'en';
+//  require_once "index_{$lang}.php"; 
 
+// exit($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+
+echo 'homepage';
 ?>
